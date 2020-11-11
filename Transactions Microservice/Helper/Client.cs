ae01d11fa373d9a366d01973bb2f7cf0c49c7a41
Vimal Adhikari 849367 <6286140-vimal_adhikari@users.noreply.gitlab.com>
2020-11-11 20:59:26 +0530

    cpy

---
+++ Transactions Microservice/Helper/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Transactions_Microservice.Helper
+{
+    public class Client
+    {
+        public HttpClient AccountDetails()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("http://40.88.222.17");
+            return client;
+        }
+
+        public HttpClient RuleApi()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("http://20.185.72.240");
+            return client;
+        }
+    }
+}
